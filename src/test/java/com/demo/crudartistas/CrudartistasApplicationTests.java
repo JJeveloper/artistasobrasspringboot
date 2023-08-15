14f5bea4438c33dc2104539e97bac811c0e992cb
Jarood <jaroodjimili@gmail.com>
2023-08-14 22:36:19 -0500

    iniciando proyecto

---
+++ src/test/java/com/demo/crudartistas/CrudartistasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.crudartistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudartistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
