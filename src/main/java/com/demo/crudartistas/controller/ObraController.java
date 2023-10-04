67b36962285973c58966acb7aaf204a402ee42ef
Jarood <jaroodjimili@gmail.com>
2023-10-04 13:25:02 -0500

    editar obra y eliminar obra listo

--- src/main/java/com/demo/crudartistas/controller/ObraController.java
+++ src/main/java/com/demo/crudartistas/controller/ObraController.java
@@ -1,7 +1,9 @@
 package com.demo.crudartistas.controller;
 
 import com.demo.crudartistas.model.Obra;
+import com.demo.crudartistas.model.TipoObra;
 import com.demo.crudartistas.service.ObraSevice;
+import com.demo.crudartistas.service.TipoObraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +21,21 @@ public class ObraController {
     @Autowired
     ObraSevice obraSevice;
 
+    @Autowired
+    TipoObraService tipoObraService;
+
     @PostMapping("/createobra")
     public String guardarObra(@ModelAttribute("obr")  Obra obra){
         obraSevice.saveObra(obra);
         return "redirect:/tableartista";
+    }
+
+    @PostMapping("/editobra")
+    public String editarObra(Model model,Obra obra){
+        obraSevice.saveObra(obra);
+        List<Obra> obraudt = obraSevice.getObraIdAutor(obra.getAutor().getId());
+        model.addAttribute("tableobra", obraudt);
+        return "verobras";
     }
 
     @GetMapping("/tableverobra/{id}")
@@ -30,6 +43,28 @@ public class ObraController {
        List<Obra> obra = obraSevice.getObraIdAutor(id);
         model.addAttribute("tableobra", obra);
         return "verobras";
+    }
+
+    @GetMapping("/formeditarobra/{id}")
+    public String formularioeditarobra(Model model, @PathVariable int id){
+
+        Obra obra = obraSevice.getById(id).get();
+
+        List<TipoObra> tipoObraList = tipoObraService.getTipoObra();
+
+        model.addAttribute("obraupd", obra);
+
+        model.addAttribute("combotipodeobra", tipoObraList);
+        return "editarobra";
+    }
+
+    @GetMapping("/deleteobra/{id}")
+    public String eliminarObra(@PathVariable int id){
+        obraSevice.deleteById(id);
+
+
+
+        return "redirect:/tableartista";
     }
 
 }
